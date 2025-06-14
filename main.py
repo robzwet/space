ed158fc25f2205deb701dd28a5c1f5e3fa584ead
robzwet <robzwet@gmail.com>
2025-06-14 02:55:29 +0200

    CH2 L5

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock = pygame.time.Clock
+	clock = pygame.time.Clock()
+	player = Player(SCREEN_HEIGHT / 2 , SCREEN_WIDTH / 2)
 	dt = 0
 
 	while True:
@@ -13,9 +15,12 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 			
-		screen.fill((0,0,0))
+		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
-		dt = pygame.tick(60) / 1000
+	
+		# limit the framerate to 60 FPS
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,8 +1,14 @@
-from circleshape import *
+import pygame
+from constants import *
+from circleshape import CircleShape
 
 class Player(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
